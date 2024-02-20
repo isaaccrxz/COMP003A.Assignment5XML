1e734407f8a6e71e81985f0498ea8da56e32bb2b
isaaccrxz <157342207+isaaccrxz@users.noreply.github.com>
2024-02-20 15:58:35 -0800

    Add project files.

---
+++ COMP003A.Assignment5XML/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Author: Isaac Cruz
+ * Course: COMP003A 
+ * Purpose: Methods and XML comments
+ */
+namespace COMP003A.Assignment5XML
+{
+    internal class Program
+    {
+        static void Main(string[] args) { }
+
+        static void PrintSeparator()
+        {
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("".PadRight(50, '*'));
+        }
+        static void PrintSeparator(string parameter)
+        {
+            PrintSeparator();
+            Console.WriteLine($"Hello, {parameter}");
+            PrintSeparator();
+            /// output string parameter
+        }
+        static void IsoscelesTriangleBuilder(char inputCharacter, int size)
+        {
+            Console.WriteLine($")
+        }
+
+    }
+}

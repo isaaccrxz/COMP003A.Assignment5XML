1502e54f26ec38b99366fb5f1357cde8a3b8e812
isaaccrxz <157342207+isaaccrxz@users.noreply.github.com>
2024-02-20 23:46:54 -0800

    second commit

--- COMP003A.Assignment5XML/Program.cs
+++ COMP003A.Assignment5XML/Program.cs
@@ -7,7 +7,29 @@ namespace COMP003A.Assignment5XML
 {
     internal class Program
     {
-        static void Main(string[] args) { }
+        static void Main(string[] args)
+        {
+            PrintSeparator();
+            Console.WriteLine("Insert a single character input: ");
+            string input = Console.ReadLine();
+            int userInput = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Insert a positive whole number: ");
+            string output = Console.ReadLine();
+            int Numberinput = Convert.ToInt32(Console.ReadLine());
+            PrintSeparator();
+
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("Favorite Characters Section");
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Michael J. Fox turns 63 this year!");
+            Console.WriteLine("Dom Torreto turns 48 this year!");
+            Console.WriteLine("Adam Sandler turns 58 this year!");
+            Console.WriteLine("Jack Black turns 55 this year!");
+            Console.WriteLine("Chris Bumstead turned 29 this year!");
+
+        }
 
         static void PrintSeparator()
         {
@@ -17,14 +39,35 @@ namespace COMP003A.Assignment5XML
         static void PrintSeparator(string parameter)
         {
             PrintSeparator();
-            Console.WriteLine($"Hello, {parameter}");
+            Console.WriteLine(parameter);
             PrintSeparator();
             /// output string parameter
         }
         static void IsoscelesTriangleBuilder(char inputCharacter, int size)
         {
-            Console.WriteLine($")
-        }
+            int x = 1;
+            while (size < 1)
+            {
+                Console.WriteLine(size);
+                x++;
+            }
+            Console.WriteLine($"{size}");
 
+            int y = -1;
+            while (size < 1)
+            {
+                Console.WriteLine(size);
+                x--;
+            }
+            Console.WriteLine($"{size}");
+            /// trying to do step 5 as best as i can
+        }
+        static void AgeCalculator(int year)
+        {
+            for (int current = 2023; current < year; current--)
+            {
+                Console.WriteLine(current);
+            }
+        }
     }
 }
